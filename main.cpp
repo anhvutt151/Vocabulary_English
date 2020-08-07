6e6f85a230504f2adf05e48504e1b8394e396ab9
anhvutt151 <anhvu15012k@gmail.com>
2020-08-08 04:03:39 +0700

    upgrade TA123
    upgrade not full

--- main.cpp
+++ main.cpp
@@ -7,7 +7,33 @@
 using namespace std;
 
 vector<string> tuVung, giaiNghia ;
+int lengthWord ;
+
+void nhapDataTA123(){
+    tuVung.clear();
+    giaiNghia.clear();
+    ifstream fileTuVung("TA123-english.txt");
+    ifstream fileGiaiNghia("TA123-vietnamese.txt");
+    if(fileTuVung.is_open()){
+        while(!fileTuVung.eof()){
+                string word;
+            getline(fileTuVung,word);
+            tuVung.push_back(word);
+        }
+    }
+    if(fileGiaiNghia.is_open()){
+        while(!fileGiaiNghia.eof()){
+                string word;
+            getline(fileGiaiNghia,word);
+            giaiNghia.push_back(word);
+        }
+    }
+    lengthWord = tuVung.size();
+}
+
 void nhapDataTCADK(){
+    tuVung.clear();
+    giaiNghia.clear();
     ifstream fileTuVung("TCA-english.txt");
     ifstream fileGiaiNghia("TCA-vietnamese.txt");
     if(fileTuVung.is_open()){
@@ -24,12 +50,12 @@ void nhapDataTCADK(){
             giaiNghia.push_back(word);
         }
     }
+    lengthWord = tuVung.size();
 }
 
 int choseIndexWord(){
-    int len = tuVung.size();
     srand(time(0));
-    int indexRandom = rand() % len ;
+    int indexRandom = rand() % lengthWord ;
     return indexRandom;
 }
 
@@ -46,7 +72,8 @@ void Screen(){
         while(true)
         {
             cout << "\n\tViet lai tu lan 1: " ;
-            cin >> vietLaiWord ;
+
+            getline(cin,vietLaiWord);
             if(vietLaiWord == word)
                 break;
             cout << "\tViet SAI! Moi viet lai: " ;
@@ -54,13 +81,12 @@ void Screen(){
          while(true)
         {
             cout << "\n\tViet lai tu lan 2: " ;
-            cin >> vietLaiWord ;
+            getline(cin,vietLaiWord);
             if(vietLaiWord == word)
                 break;
             cout << "\n\tViet SAI! Moi viet lai:" ;
         }
         cout << "\n\tViet NGHIA cua tu: " ;
-        cin.ignore();
         getline(cin,vietNghia);
         if(vietNghia != nghia) cout << "\n\tBan Sai!";
         else cout << "\n\tBan Dung!" ;
@@ -80,15 +106,48 @@ void Screen(){
         char playAgain ;
         cout << "\n\tBan muon choi tiep khong? (Y/N) " ;
         cin >> playAgain ;
-        if(playAgain == 'n' || playAgain == 'Y') break;
+        cin.ignore();
+        if(playAgain == 'n' || playAgain == 'N') break;
         else continue;
         system("pause");
     }
 }
 
+void MenuApp(){
+    while(true){
+        system("cls");
+        cout << "\t\tMenu List Word";
+        cout << "\n\t1: TCA-Words";
+        cout << "\n\t2: TA123-Words";
+        cout << "\n\t0: Thoat!";
+        char select;
+        while(true){
+            cout << "\n\tLua chon cua ban: ";
+            cin >> select;
+            cin.ignore();
+            if(select == '1'){
+                nhapDataTCADK();
+                Screen();
+                break;
+            }
+            else if(select == '2'){
+                nhapDataTA123();
+                Screen();
+                break;
+            }
+            else if(select == '0')
+                return;
+            continue;
+        }
+
+        system("pause");
+    }
+
+}
+
 int main()
 {
-    nhapDataTCADK();
-    Screen();
+    MenuApp();
+
     return 0;
 }
